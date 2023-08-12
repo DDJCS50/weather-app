3e343566e51f01953b70f462df5773452b808435
djns <130430285+DDJCS50@users.noreply.github.com>
2023-08-12 00:22:45 -0700

    Add temperature type toggle
    Added funcs that allow the user to see temperature in celsius or in
    fahrenheit based on their selection.

---
+++ src/log.js
@@ -0,0 +1,8 @@
+const tempType = (userSelection) => {
+  if (userSelection === "Celsius") {
+    return "c";
+  }
+  return "f";
+};
+
+export default tempType;
--- src/page.js
+++ src/page.js
@@ -1,7 +1,9 @@
+import tempType from "./log";
+
 const main = document.querySelector("main");
 
 const home = () => {
-  function callServer(city) {
+  function callServer(city, type) {
     const display = document.querySelector("p");
     const myKey = "bc3ae431310649bdaf2155956230508";
     async function getTemp() {
@@ -12,7 +14,11 @@ const home = () => {
         );
         const tempData = await response.json();
         console.log(tempData);
-        display.innerText = `${tempData.current.temp_f}\xB0 Fahrenheit, ${tempData.current.temp_c}\xB0 Celsius in ${tempData.location.name}, ${tempData.location.region}, ${tempData.location.country}`;
+        if (type === "c") {
+          display.innerText = `${tempData.current.temp_c}\xB0 Celsius in ${tempData.location.name}, ${tempData.location.region}, ${tempData.location.country}`;
+        } else {
+          display.innerText = `${tempData.current.temp_f}\xB0 Fahrenheit in ${tempData.location.name}, ${tempData.location.region}, ${tempData.location.country}`;
+        }
         if (tempData.current.temp_f > 70) {
           main.style.backgroundColor = "coral";
         } else {
@@ -29,8 +35,18 @@ const home = () => {
 
   function makePage() {
     const newContainer = document.createElement("div");
+    const promptUser = document.createElement("h3");
+    promptUser.innerText = "Enter a City Location here:";
     newContainer.classList = "holder";
     const cityInput = document.createElement("input");
+    cityInput.placeholder = "City";
+    const tempDropdown = document.createElement("select");
+    const optionF = document.createElement("option");
+    optionF.value = "Fahrenheit";
+    optionF.innerText = "Fahrenheit";
+    const optionC = document.createElement("option");
+    optionC.value = "Celsius";
+    optionC.innerText = "Celsius";
     const searchBtn = document.createElement("button");
     const searchForm = document.createElement("form");
     searchForm.method = "#";
@@ -39,8 +55,12 @@ const home = () => {
     cityInput.type = "search";
     const newDisplay = document.createElement("p");
 
+    tempDropdown.appendChild(optionC);
+    tempDropdown.appendChild(optionF);
+    searchForm.appendChild(tempDropdown);
     searchForm.appendChild(cityInput);
     searchForm.appendChild(searchBtn);
+    newContainer.appendChild(promptUser);
     newContainer.appendChild(searchForm);
     newContainer.appendChild(newDisplay);
     main.appendChild(newContainer);
@@ -50,7 +70,7 @@ const home = () => {
       let citySelect = "";
       if (cityInput.value.match(/^[a-zA-Z ]*$/)) {
         citySelect = cityInput.value;
-        callServer(citySelect);
+        callServer(citySelect, tempType(tempDropdown.value));
       }
     });
   }
